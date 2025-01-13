b6552f89478ce2ad786aa05881cd4591c2060ab8
king <king@163.com>
2025-01-13 20:21:59 +0800

    init

--- assets/javascripts/discourse/api-initializers/hide-title.js
+++ assets/javascripts/discourse/api-initializers/hide-title.js
@@ -4,7 +4,19 @@ export default {
   name: "hide-topic-title",
 
   initialize() {
-    withPluginApi("0.8.13", (api) => {
+    withPluginApi("0.8", (api) => {
+      api.decorateCookedElement(
+          (cooked, helper) => {
+            console.log("-----333---")
+            if (helper.controllerName === "topic") {
+              const title = cooked.querySelector(".quote .title>a");
+              if (title && /t-bot-\d{13,13}/.test(title.innerText)) {
+                title.style.display = "none";
+              }
+            }
+          }
+      );
+
       api.modifyClass("component:topic-list-item", {
         pluginId: "hide-topic-title",
         didInsertElement() {
@@ -12,7 +24,7 @@ export default {
           console.log('topic-进入插件内部')
           const title = this.element.querySelector(".topic-title");
 
-          if(/t-bot-\d{13,13}/.test(title.innerText)){
+          if(title && /t-bot-\d{13,13}/.test(title.innerText)){
             console.log('topic-插件匹配通过')
             title.style.display = "none";
           }
@@ -24,13 +36,13 @@ export default {
         // 判断是否是主题详情页
         if (window.location.pathname.includes("/t/")) {
           const topicTitle = document.querySelector(".title-wrapper h1");
-          if(/t-bot-\d{13,13}/.test(topicTitle.innerText)){
+          if(topicTitle && /t-bot-\d{13,13}/.test(topicTitle.innerText)){
             console.log('topic-detail-插件匹配通过')
             topicTitle.style.display = "none"; // 隐藏标题
           }
           const quotedTitle = document.querySelector(".quote .title>a");
           console.log('topic-quoted-检查是否为空 ' + quotedTitle)
-          if(/t-bot-\d{13,13}/.test(quotedTitle.innerText)){
+          if(quotedTitle && /t-bot-\d{13,13}/.test(quotedTitle.innerText)){
             console.log('topic-quoted-插件匹配通过')
             quotedTitle.style.display = "none"; // 隐藏标题
           }
